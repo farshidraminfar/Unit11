daaa1b2083a0b2f3da607480cbc416f2420945f5
farshidraminfar <raminfar_farshid@student.smc.edu>
2017-06-16 18:34:40 -0700

    Add files via upload

---
+++ websvr.js
@@ -0,0 +1,18 @@
+var http = require("http");
+
+http.createServer(function (request, response) {
+   
+   response.writeHead(200, {'Content-Type': 'text/html'});
+
+response.write('Hello World, my name is Farshid Raminfar');
+}).listen(8001);
+
+console.log('Server running at http://127.0.0.1:8001/');
+
+
+
+
+
+
+
+
